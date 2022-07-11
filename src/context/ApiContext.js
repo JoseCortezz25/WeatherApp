b13d35e53a28bfe55a239884f532a49c2fcc3449
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2022-07-11 15:07:13 -0500

    Added responsive design

--- src/context/ApiContext.js
+++ src/context/ApiContext.js
@@ -4,9 +4,11 @@ const APIContext = createContext({})
 
 export const ApiContextProvider = ({ children }) => {
   const [data, setData] = useState(null)
+  // const [coordinates, setCoordinates] = useState([])
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
+      // setCoordinates([position.coords.latitude, position.coords.longitude])
       fetch(`${process.env.REACT_APP_API}onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
         .then((response) => response.json())
         .then((weather) => {
